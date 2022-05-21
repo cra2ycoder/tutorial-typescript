84ad8b7c0f5008ae90cfbea3e340f4f7148ee23f
mohaa <mohaa.r.6789@gmail.com>
2022-05-21 20:47:45 +0530

    dom and type aliases added

---
+++ code/domAndTypeAliases.ts
@@ -0,0 +1,7 @@
+const anchorTag = document.querySelector('a')
+
+console.log(anchorTag.href)
+
+const headingTag = document.querySelector('h1') as HTMLHeadingElement
+
+const formTag: HTMLFormElement = document.querySelector('form')
