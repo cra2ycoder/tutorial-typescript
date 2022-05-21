bad829469272ae682ca8a4c41409b424d4b9f40e
mohaa <mohaa.r.6789@gmail.com>
2022-05-21 20:58:13 +0530

    prettier applied to class

--- code/class.ts
+++ code/class.ts
@@ -4,55 +4,55 @@
  *
  */
 
-type IPrimitiveType = number | string;
+type IPrimitiveType = number | string
 
 interface IAnimal {
-  name: string;
-  category: string;
-  height: IPrimitiveType;
-  weight: IPrimitiveType;
+  name: string
+  category: string
+  height: IPrimitiveType
+  weight: IPrimitiveType
 
-  getName(): string;
-  setDetails(width: IPrimitiveType, height: IPrimitiveType): void;
+  getName(): string
+  setDetails(width: IPrimitiveType, height: IPrimitiveType): void
 }
 
 class Animal implements IAnimal {
-  public name: string;
-  public category: string;
-  public weight: IPrimitiveType;
-  public height: IPrimitiveType;
+  public name: string
+  public category: string
+  public weight: IPrimitiveType
+  public height: IPrimitiveType
 
   // readonly
-  readonly age: IPrimitiveType;
+  readonly age: IPrimitiveType
 
   constructor(name?: string) {
-    this.name = name;
+    this.name = name
   }
 
   private init() {
-    console.log("___internal init method__");
+    console.log('___internal init method__')
   }
 
   protected getDetails() {
     return {
       weight: this.weight,
-      height: this.height
-    };
+      height: this.height,
+    }
   }
 
   public getName() {
-    return this.name;
+    return this.name
   }
 
   public setDetails(weight: IPrimitiveType, height: IPrimitiveType) {
-    this.weight = weight;
-    this.height = height;
+    this.weight = weight
+    this.height = height
   }
 }
 
-const dog = new Animal("puppy");
-dog.getName();
-dog.setDetails(100, 200);
+const dog = new Animal('puppy')
+dog.getName()
+dog.setDetails(100, 200)
 
 /**
  * @description
