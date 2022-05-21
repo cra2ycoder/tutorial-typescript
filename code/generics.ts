c8afd594552297047e6a428e89a23b7d7214a037
mohaa <mohaa.r.6789@gmail.com>
2022-05-21 20:11:43 +0530

    prettier updates

--- code/generics.ts
+++ code/generics.ts
@@ -5,75 +5,75 @@
 
 // basics
 function simpleGeneric<T>(arg: T): T {
-  return arg;
+  return arg
 }
 
-const stringData = simpleGeneric<string>("string");
+const stringData = simpleGeneric<string>('string')
 
-const numberData = simpleGeneric<number>(1);
+const numberData = simpleGeneric<number>(1)
 
-const booleanData = simpleGeneric<boolean>(true);
+const booleanData = simpleGeneric<boolean>(true)
 
-const stringArrData = simpleGeneric<string[]>([]);
+const stringArrData = simpleGeneric<string[]>([])
 
 /**
  * @description
  * INTERFACES
  */
 interface Story<T> {
-  name: string;
-  description: string;
-  info: T;
+  name: string
+  description: string
+  info: T
 }
 
 interface IStoryInfo {
-  author: string;
-  publishedOn: string;
-  publisher: string;
+  author: string
+  publishedOn: string
+  publisher: string
 }
 
 const myStory: Story<string> = {
-  name: "Harry Potter",
-  description: "Fantasy",
-  info: "Nice movie!"
-};
+  name: 'Harry Potter',
+  description: 'Fantasy',
+  info: 'Nice movie!',
+}
 
 const myNewStory: Story<IStoryInfo> = {
-  name: "The Jungle Book",
-  description: "Animation",
+  name: 'The Jungle Book',
+  description: 'Animation',
   info: {
-    author: "",
-    publishedOn: "00/00/0000",
-    publisher: ""
-  }
-};
+    author: '',
+    publishedOn: '00/00/0000',
+    publisher: '',
+  },
+}
 
 /**
  * @description
  * CLASSES
  */
 interface IBook {
-  name: string;
-  description: string;
-  publishedOn?: string;
-  publisher?: string;
+  name: string
+  description: string
+  publishedOn?: string
+  publisher?: string
 }
 
 class Book<T> {
-  private info: T;
+  private info: T
   constructor(data: T) {
-    this.info = data;
+    this.info = data
   }
 
   public getBookInfo(): T {
-    return this.info;
+    return this.info
   }
 }
 
 const myBook = new Book<IBook>({
-  name: "book",
-  description: "book description"
-});
+  name: 'book',
+  description: 'book description',
+})
 
-const bookInfo = myBook.getBookInfo();
-console.log(bookInfo.name);
+const bookInfo = myBook.getBookInfo()
+console.log(bookInfo.name)
